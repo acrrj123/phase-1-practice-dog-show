3ce581de5c1954208144d9b23d68b13a47c47d30
Anna Cole <acrrj123@gmail.com>
2023-06-07 13:21:25 -0500

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  
   fetch('http://localhost:3000/dogs')
   .then(resp => resp.json())
   .then(dogsArray => dogsArray.forEach(dog => {
     renderDogs(dog) 
     let dogId = dog.id
-   return dogId }
-   ))
-   
-  const form = document.getElementById('dog-form')
+  }))
   
-  function renderDogs(dog, dogId) {
-    const table = document.getElementById('table-body')
+  const form = document.getElementById('dog-form')
+  const table = document.getElementById('table-body')
+  
+  function renderDogs(dog) {
     const tr = document.createElement('tr')
     table.appendChild(tr)
     const tdName = document.createElement('td')
@@ -36,42 +34,37 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  // let obj = {}
-
-  // form.addEventListener('change', (e) => {
-  //   e.preventDefault()
-  //   const obj = {
-  //     name: e.target.parentElement.name.value,
-  //     breed: e.target.parentElement.breed.value,
-  //     sex: e.target.parentElement.sex.value
-  //   }
-  //   return obj
-  // })
-
   form.addEventListener ('submit', (e, dogId) => {
     //console.log(e.target)
     //console.log(form)
     e.preventDefault()
-    const obj = {
+    const newDogObj = {
       name: e.target.name.value,
       breed: e.target.breed.value,
       sex: e.target.sex.value
     }
-    updateDog(e.target.name.id, obj)
+    updateDog(e.target.name.id, newDogObj)
     //console.log(e.target.name.id)
     e.target.reset()
   })
 
-  function updateDog(id, obj) {
+  function updateDog(id, newDogObj) {
     fetch(`http://localhost:3000/dogs/${id}`, { 
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json'
       },
-      body: JSON.stringify(obj)
+      body: JSON.stringify(newDogObj)
     })
     .then(resp => resp.json())
-    .then(updatedDog => renderDogs(updatedDog))
+    .then(newDogObj => {
+      //console.log(newDogObj)
+      table.innerHTML = ''
+
+      fetch('http://localhost:3000/dogs')
+      .then(resp => resp.json())
+      .then(updatedDogsArray => updatedDogsArray.forEach(updatedDog => renderDogs(updatedDog)))
+    })
   }
 })
